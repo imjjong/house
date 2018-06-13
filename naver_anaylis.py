3aa085c7927ba5c40faad46e741e4b1fa8e77fdf
imjjong <lvzerovl@naver.com>
2018-06-14 07:25:23 +0900

    Create naver_anaylis.py

---
+++ naver_anaylis.py
@@ -0,0 +1,29 @@
+
+import re
+
+INPUT_FILE_NAME = 'target.txt'
+OUTPUT_FILE_NAME = 'output_cleand.txt'
+
+
+def clean_text(text):
+    cleaned_text = re.sub('[a-zA-Z]', '', text)
+    cleaned_text = re.sub('[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]',
+                          '', cleaned_text)
+    return cleaned_text
+
+
+def main():
+    read_file = open(INPUT_FILE_NAME, 'r')
+    write_file = open(OUTPUT_FILE_NAME, 'w')
+    text = read_file.read()
+    text = clean_text(text)
+    write_file.write(text)
+    read_file.close()
+    write_file.close()
+
+
+if __name__ == "__main__":
+    main()
+
+
+    http://yoonpunk.tistory.com/7
